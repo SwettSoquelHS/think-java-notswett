8d1c60ff863cdf982cea5a2dcd47d56efa9786e5
notswett <notswetty@gmail.com>
2019-01-24 10:01:42 -0800

    New code

---
+++ arraylist/Assignment.java
@@ -0,0 +1,20 @@
+/*
+    A simple container class to hold onto 
+    the values of an assignment.
+*/
+public class Assignment {
+    protected String assignmentName;
+    protected float scoreRecieved;
+    protected float totalPoints;
+
+    /*
+        Constructor for Assignment
+    */
+    //TODO - replace this with a default constructor that will 
+    //take a string, and two floats and initialize the properties appropiatly.
+
+    //TODO - Write three getters to retrieve the values of the three properties
+    //getName()
+    //getScore()
+    //getTotalPoints()
+}
