cd5e95fd2ff4088431ffa96c49233f17cb00052f
notswett <notswetty@gmail.com>
2019-01-24 09:53:22 -0800

    New code

---
+++ arraylist/GradeBook.java
@@ -0,0 +1,54 @@
+
+/*
+    A sample program demonstrating how an 
+    ArrayList can be used to keep track of 
+    a collection of similar items.
+*/
+public class GradeBook {
+    
+    public static void main(String[] args){
+
+        //Create an instance of a gradebook to add 
+        Gradebook book1 = new Gradebook();
+
+    }
+
+    /*
+        Default constructor.
+    */
+    GradeBook(){
+
+    }
+
+    /*
+        Adds an assignment to the gradebook.
+    */
+    void addAssignment(String assignmentName, float score, float totalPointsPossible){
+
+    }
+
+    /*
+        Look for the assignment and remove it from the
+        gradebook.
+    */
+    boolean deleteAssignment(String assignmentName){
+        return false;
+    }
+
+    /*
+        Adds up the total points available, the total points
+        given for each assignment and returns the score.
+    */
+    float calculateGrade( ){
+
+    }
+
+    /*
+        Print the gradebook:
+            Assignment Name:  score/Total
+    */
+    void printGradebook(){
+
+    }
+
+}
