207cf31b46f6d15eeb289f894b3ec4c0ef3aac51
notswett <notswetty@gmail.com>
2019-01-24 10:04:19 -0800

    ChangeArrays added

--- arraylist/ChangeArrays.java
+++ arraylist/ChangeArrays.java
@@ -14,8 +14,13 @@ public class ChangeArrays {
 
         System.out.println("Current scores are: " + Arrays.toString(scores));
 
-        //TODO 1: Write code to make a new array that can hold a new
-        
+        //TODO 1: Write code to make a new array that can hold a new score
+        int[] temp = new int[scores.length + 1];
+        for(int i = 0; i<scores.length; i++){
+            temp[i] = scores[i];
+        }
+        temp[temp.length -1] = 100;
+        scores = temp;
 
 
         // ... code to add should stay above this line
@@ -24,7 +29,10 @@ public class ChangeArrays {
 
 
         //TODO 2: Next, write code to remove the first value from the scores
-
+        int[] temp2 = new int[scores.length -1];
+        for(int i = 1; i < scores.length; i++){
+            temp2[i-1] = scores[i];
+        }
 
         // ... code to remove should stay above this line
         System.out.println("After 'remove' scores are: " + Arrays.toString(scores));
@@ -35,19 +43,42 @@ public class ChangeArrays {
         //System.out.println("Copy of scores looks like: " + scores);
     }
 
-
+    /*
+        int[] first_arr = {5, 6, 7 ,8}
+        int[] second_arr = makeCopyOf(first_arr)
+        //second_arr should also have 5,6, 7, 8
+        //furthermore,
+        second_arr[1] = 12; //Should not change first_arr
+        print(first_arr) -> {5, 6, 7 ,8}
+        print(second_arr) -> {5, 12, 7 ,8}
+    */
     public static int[] makeCopyOf(int[] arr){
         return null;
     }
 
-    public static int[] addValue(int[] arr, int val){
+    /*
+        int[] arr = { 5, 6, 7 ,8 }
+        arr = addValueAt( arr, -9, 3)
+        // { 5, 6, 7, -9, 8}
+    */
+    public static int[] addValueAt(int[] arr, int val, int indexAt){
         return null;
     }
 
+   /*
+        int[] arr = { 5, 6, 7 ,8 }
+        arr = removeLast( arr)
+        // arr should look like { 5, 6, 7}
+    */
     public static int[] removeLast(int[] arr){        
         return null;
     }
 
+   /*
+        int[] arr = { 5, 6, 7 ,8 }
+        arr = removeIndex(arr, 1)
+        //arr should look like { 5, 7, 8 }
+    */
     public static int[] removeIndex(int[] arr, int index){
         return null;
     }
