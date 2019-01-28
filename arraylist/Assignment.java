f6cfdb3e682e0eb1e23706d9e579ae322cb5f4d3
notswett <notswetty@gmail.com>
2019-01-28 08:53:12 -0800

    new code

--- arraylist/Assignment.java
+++ arraylist/Assignment.java
@@ -4,17 +4,33 @@
 */
 public class Assignment {
     protected String assignmentName;
-    protected float scoreRecieved;
-    protected float totalPoints;
+    protected double scoreRecieved;
+    protected double totalPoints;
 
     /*
         Constructor for Assignment
     */
-    //TODO - replace this with a default constructor that will 
-    //take a string, and two floats and initialize the properties appropiatly.
+    Assignment(String name, double points, double totalPoints){
+        assignmentName = name;
+        scoreRecieved = points;
+        this.totalPoints = totalPoints;
+    }
 
-    //TODO - Write three getters to retrieve the values of the three properties
-    //getName()
-    //getScore()
-    //getTotalPoints()
+    
+    /*
+        Return the name of the assignment.
+    */
+    String getName(){ return assignmentName; }
+
+    /*
+        Return the score received for the assignment
+    */
+    double getScore(){return scoreRecieved; }
+
+    /* 
+    Return the points possible for the assignment
+    */
+    double getTotalPoints(){ return totalPoints; }
 }
+
+
--- arraylist/GradeBook.java
+++ arraylist/GradeBook.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 /*
     A sample program demonstrating how an 
@@ -9,22 +10,47 @@ public class GradeBook {
     public static void main(String[] args){
 
         //Create an instance of a gradebook to add 
-        Gradebook book1 = new Gradebook();
+        GradeBook book1 = new GradeBook();
+        book1.addAssignment("Exam1", 0.9 , 1.0);
+        book1.addAssignment("Exam2", 0.75, 1.0);
+        book1.addAssignment("Quiz" , 0.2 , 0.25);
 
+        book1.printGradebook();
+        System.out.println("Current grade is: " + book1.calculateGrade());
+
+        System.out.println("Removing Exam2...");
+        book1.deleteAssignment("Exam2");
+        book1.printGradebook();
+        System.out.println("Current grade is: " + book1.calculateGrade());
     }
+
+    /* Gradbook Properties */
+    ArrayList assignments;
 
     /*
         Default constructor.
     */
     GradeBook(){
+        assignments = new ArrayList();
+    }
 
+    void deleteLessthan(double thresh){
+        for(int i=assignment.size()-1; i >= 0; i--){
+            Assignment asg = (Assignment)assignments.get(i);
+            double calcScore = asg.getScore()/asg.getTotalPoints();
+            if( calcScore < thresh){
+                assignments.remove(i);
+            }
+        }
     }
 
     /*
         Adds an assignment to the gradebook.
     */
-    void addAssignment(String assignmentName, float score, float totalPointsPossible){
-
+    void addAssignment(String assignmentName, double score, double totalPointsPossible){
+        Assignment newAssignment = new Assignment(assignmentName,
+            score, totalPointsPossible ); 
+        assignments.add( newAssignment );
     }
 
     /*
@@ -32,15 +58,44 @@ public class GradeBook {
         gradebook.
     */
     boolean deleteAssignment(String assignmentName){
-        return false;
+        boolean result = false;
+        int deleteAt = -1;
+        for(int i = 0; i < assignments.size(); i++){
+            Assignment asg = (Assignment)assignments.get(i);
+            if( assignmentName.equals(asg.getName()) ){
+                deleteAt = i;
+                break;
+            }
+        }
+
+        if( deleteAt >= 0){
+            result = true;
+            assignments.remove(deleteAt);
+        }
+
+        return result;
     }
 
     /*
         Adds up the total points available, the total points
         given for each assignment and returns the score.
     */
-    float calculateGrade( ){
+    double calculateGrade( ){
+        double result = -1;
+        double myScore = 0;
+        double totalScore = 0;
 
+        //Loop over assignments and add up the points
+        for(Object item: assignments){
+            Assignment asg = (Assignment)item;
+            myScore += asg.getScore();
+            totalScore += asg.getTotalPoints();
+        }
+
+        if(totalScore > 0)
+            result = myScore / totalScore;
+
+        return result;
     }
 
     /*
@@ -48,6 +103,13 @@ public class GradeBook {
             Assignment Name:  score/Total
     */
     void printGradebook(){
+        for(Object o: assignments){
+            Assignment asg = (Assignment)o;
+            //asg.getName()
+            //    The Assignment name:  # / #
+            System.out.println("\t" + asg.getName() + ": " + asg.getScore() +
+                " / " + asg.getTotalPoints());
+        }
 
     }
 
