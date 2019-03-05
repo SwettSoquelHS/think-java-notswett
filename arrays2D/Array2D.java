a382d1dde61f8677462a0e7224406b58cccb9d3b
notswett <notswetty@gmail.com>
2019-03-05 09:44:03 -0800

    adding array stuff

---
+++ arrays2D/Array2D.java
@@ -0,0 +1,16 @@
+public class Array2D {
+    
+    public static void main(String[] args){
+        //Normal 1-dimensional array
+        int[] arr1 = {1,2,3,};
+
+        //2-Dimensional array
+        int[][] arr2;
+
+    }
+
+    public static void print2DArray(int[][] matrix){
+
+
+    }
+}
